bc80caa06e721c58666280366f5ce0ab3eee379f
Solpostgit <begemot_off@mail.ru>
2023-08-07 20:14:18 +0300

    Task1 completed

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
+        task2();/*
         task3();
         task4();
         task5();
         task6();
-        task7();
+        task7(); */
     }
     public static void task1 () {
         System.out.println("task1");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", он совершеннолетний");
+        }
+        else System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        System.out.println();
+    }
+
+    public static void task2 () {
+        System.out.println("task2");
 
 
     }
+
 
 }
